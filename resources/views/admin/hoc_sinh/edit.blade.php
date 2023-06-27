b057fe29c1e3a92a8a18fefeecd6d8fb4cc26aa5
xaocho0987 <63099187+xaocho0987@users.noreply.github.com>
2023-06-27 15:31:26 +0700

    up

--- resources/views/admin/hoc_sinh/edit.blade.php
+++ resources/views/admin/hoc_sinh/edit.blade.php
@@ -1,7 +1,7 @@
 <form method="post" action="/hoc_sinh/{{$hocSinh->id}}">
     @csrf
     @method('PUT')
-    <label for="HoTen">Họ tên:</label>
-    <input type="text" name="HoTen" value="{{ $hocsinh->ho_ten }}" required>
+    <label for="ho_ten">Họ tên:</label>
+    <input type="text" name="ho_ten" value="{{ $hocsinh->ho_ten }}" required>
     <button type="submit">Cập nhật</button>
 </form>
--- resources/views/admin/hoc_sinh/index.blade.php
+++ resources/views/admin/hoc_sinh/index.blade.php
@@ -14,27 +14,28 @@
                     <table class="table">
                         <thead>
                             <tr>
-                                <th>STT</th>
+                                <th>ID</th>
                                 <th>Họ tên</th>
                                 <th>Thời gian cập nhật</th>
                                 <th>Thao tác</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php $index = 0; ?>
+                            {{ $index = 0 }}
                             @foreach ($hocSinhs as $hocSinh)
-                            <?php $index += 1; ?>
+                            {{ $index += 1 }}
                                 <tr>
                                     <td>{{ $index }}</td>
+                                    <td>{{ $hocSinh->id }}</td>
                                     <td>{{ $hocSinh->HoTen }}</td>
                                     <td>{{ $hocSinh->ThoiGianCapNhap }}</td>
                                     <td>
-                                        <a href="{{ route('hoc_sinh.edit', $hocSinh->id) }}" class="btn btn-sm btn-primary">Sửa</a>
-                                        <form action="{{ route('hoc_sinh.destroy', $hocSinh->id) }}" method="POST" style="display: inline-block">
+                                        {{-- <a href="{{ route('hoc_sinh.edit', $hocSinh->ID) }}" class="btn btn-sm btn-primary">Sửa</a> --}}
+                                        {{-- <form action="{{ route('hoc_sinh.destroy', $hocSinh->ID) }}" method="POST" style="display: inline-block">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Bạn có chắc muốn xoá?')">Xoá</button>
-                                        </form>
+                                        </form> --}}
                                     </td>
                                 </tr>
                             @endforeach
